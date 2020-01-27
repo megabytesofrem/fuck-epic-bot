5f83508d262eeac678edff37617f4d2d717eb157
dhilln <dhillonwilk@gmail.com>
2020-01-27 01:16:23 -0800

    Fixed require in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const _ = require('lodash'); // for sample
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const config = require('./responses.json');
+const config = require('./config.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
